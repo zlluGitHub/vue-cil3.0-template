9c8e41f7b0716b4f7bc7380c853b2587c3b9a82a
zlluGitHub <18230086651@163.com>
2019-09-17 11:28:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
+//挂载 api
 import axios from 'axios';
 import qs from 'qs';
+axios.defaults.baseURL = process.env.VUE_APP_URL;
+Vue.prototype.$axios = axios;  
+Vue.prototype.$qs = qs;  
 
 import router from './router/index';
 import store from './store/index';
-
-Vue.prototype.$axios = axios;  
-Vue.prototype.$qs = qs;    
-
-Vue.config.productionTip = false
+  
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
   router,
   store
-}).$mount('#app')
+}).$mount('#app');
