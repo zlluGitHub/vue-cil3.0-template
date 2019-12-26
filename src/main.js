2e8cbe7600cee68d1624bb44316ee25573cf8648
zlluGitHub <18230086651@163.com>
2019-12-26 16:10:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,29 +1,34 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
 
-// 全局挂载 axios
-import axios from 'axios';
-import qs from 'qs';
-axios.defaults.baseURL = process.env.VUE_APP_URL;
-Vue.prototype.$axios = axios;  
-Vue.prototype.$qs = qs;  
+import 'font-awesome/css/font-awesome.css'
 
-// 全局挂载 ViewUI
-import ViewUI from 'view-design';
+//引入 iview
+import iView from 'view-design';
 import 'view-design/dist/styles/iview.css';
-Vue.use(ViewUI);
+Vue.use(iView);
 
-// 全局挂载 echarts
+// 引入echarts
 import echarts from 'echarts'
-Vue.prototype.$echarts = echarts; 
+Vue.prototype.$echarts = echarts;
 
-import router from './router/index';
-import store from './store/index';
-  
-Vue.config.productionTip = false;
+//引入axios
+// import apiConfig from '../config/api.config.js'
+import axios from 'axios'
+import qs from 'qs'
+// axios.defaults.baseURL = apiConfig.baseUrl
+Vue.prototype.$axios = axios    //全局注册，使用方法为:this.$axios
+Vue.prototype.$qs = qs           //全局注册，使用方法为:this.$qs
+
+//实例化 store
+import store from './store'
+
+import router from './router'
+
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
   router,
-  store
-}).$mount('#app');
+  store,
+  render: h => h(App),
+}).$mount('#app')
