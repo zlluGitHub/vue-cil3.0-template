07b8d0daecf81f1f01e0d386fbc077ef3a3936a8
zlluGitHub <18230086651@163.com>
2020-03-26 13:17:21 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Section from '../components/content/Section.vue';
 
 Vue.use(Router)
 
@@ -11,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Section',
-      component: Section
+      component: () => import('../components/content/Section.vue')
     },
     // {
     //   path: '/second',
