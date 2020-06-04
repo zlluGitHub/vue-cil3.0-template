de8f39cb469db2d63a9a321250f6e8c546689ac4
zhenglinglu <l>
2020-06-04 10:00:22 +0800

    优化目录结构

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,36 @@
 <template>
-  <div>
-    <Header />
-    <!-- <Sidebar /> -->
-    <router-view/>
-    <Footer />
+  <div class="main">
+    <Header></Header>
+    <div class="warp">
+      <Sidebar></Sidebar>
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Footer from "./compon/Footer";
+// import Footer from "./compon/Footer";
 import Header from "./compon/Header";
-// import Sidebar from "./compon/Sidebar";
+import Sidebar from "./compon/Sidebar";
 export default {
   name: "footers",
   components: {
-    Footer,
-    // Sidebar,
+    // Footer,
+    Sidebar,
     Header
   }
 };
 </script>
 
+<style lang="scss" scoped>
+.main {
+  .warp {
+    display: flex;
+    .content {
+      flex-grow: 1;
+    }
+  }
+}
+</style>
--- src/components/compon/Header.vue
+++ src/components/compon/Header.vue
@@ -1,15 +1,44 @@
 <template>
-  <header>
-    header
-  </header>
+  <Header>
+    <Menu mode="horizontal" theme="dark" active-name="1">
+      <div class="layout-logo"></div>
+      <div class="layout-nav">
+        <MenuItem name="1">
+          <Icon type="ios-navigate"></Icon>Item 1
+        </MenuItem>
+        <MenuItem name="2">
+          <Icon type="ios-keypad"></Icon>Item 2
+        </MenuItem>
+        <MenuItem name="3">
+          <Icon type="ios-analytics"></Icon>Item 3
+        </MenuItem>
+        <MenuItem name="4">
+          <Icon type="ios-paper"></Icon>Item 4
+        </MenuItem>
+      </div>
+    </Menu>
+  </Header>
 </template>
 
 <script>
 export default {
-  name: "headers",
-  props: {
-    msg: String
+  data() {
+    return {
+     
+     
+    };
+  },
+  computed: {
+    // rotateIcon() {
+    //   return ["menu-icon", this.isCollapsed ? "rotate-icon" : ""];
+    // }
+  },
+  methods: {
+    // collapsedSider() {
+    //   this.$refs.side1.toggleCollapse();
+    // }
   }
 };
 </script>
+
 
